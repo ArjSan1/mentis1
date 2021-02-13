e1caa064ccec45c639ccde0cdc9f0b272293844c
Arjun Santhanakrishnan <arjun.santhan@gmail.com>
2021-02-13 17:27:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.use(express.static('views'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/landingPage/index.html');
+});
+
+/*app.listen(8000, () => {
+    console.log('server started');
+});*/
+
+server.listen(3000, () => console.log(`Listening on port 3000`));
