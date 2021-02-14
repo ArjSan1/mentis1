0b65453ad925e4cd46b55e5781072ec491dbf421
Arjun <arjun.santhan@gmail.com>
2021-02-14 02:37:07 +0000

    Added journal button to get journal text

---
+++ views/journal/journal.js
@@ -0,0 +1,13 @@
+let textBox = document.getElementById("journalInput");
+let box = document.getElementsByClassName("dataEntry");
+let pStyle = document.getElementByC
+let text;
+function submitText(){
+  
+ text = textBox.value;
+ textBox.value="";
+
+
+
+
+}
